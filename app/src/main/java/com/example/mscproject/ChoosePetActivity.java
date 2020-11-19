d72b386b57df7dc0e7d4af94d61c9cf3e8fb109e
Sarah Sidders <ssidders@outlook.com>
2020-11-19 20:52:47 +0000

    Removed text and added toast popup messages

--- app/src/main/java/com/example/mscproject/ChoosePetActivity.java
+++ app/src/main/java/com/example/mscproject/ChoosePetActivity.java
@@ -4,10 +4,15 @@ import android.content.ContentValues;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.os.Handler;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.Toast;
+import androidx.annotation.StringRes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import static com.example.mscproject.DatabaseHelper.PET_TABLE;
@@ -17,38 +22,96 @@ public class ChoosePetActivity extends AppCompatActivity {
     private PetType petType;
     private String petName;
     private EditText etInputName;
+    private boolean hasName;
+    private boolean hasType;
+
+    private Button submitBtn;
+
 
     public ChoosePetActivity (){
     }
 
+    public void setButtonVisibility() {
+        if (hasName == true && hasType == true) {
+            submitBtn.setVisibility(View.VISIBLE);
+        }
+        else {
+            submitBtn.setVisibility(View.INVISIBLE);
+        }
+    }
+
+
     // Choosing the pet type
     public void onPetClick(View view) {
 
-        boolean checked = ((RadioButton) view).isChecked();
+        hasType = ((RadioButton) view).isChecked();
         switch(view.getId()) {
             case R.id.choose_rb_bront:
-                if (checked)
+                if (hasType)
                     petType = PetType.Bronchiosaurus;
                     break;
             case R.id.choose_rb_trex:
-                if (checked)
+                if (hasType)
                     petType = PetType.Trex;
                     break;
             case R.id.choose_rb_tri:
-                if (checked)
+                if (hasType)
                     petType = PetType.Triceratops;
                     break;
         }
+        setButtonVisibility();
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_pet);
+        submitBtn = findViewById(R.id.choose_btnSubmit);
+        setButtonVisibility();
 
+
+        Toast.makeText(this, "Welcome to Jurassic Pets. Please enter a name for your pet above", Toast.LENGTH_LONG).show();
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                Toast.makeText(getApplicationContext(), "Then click on the pet that you would like to choose", Toast.LENGTH_LONG).show();
+            }
+        }, 4000);
+
+
+
+
+        TextWatcher textWatcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (editable.length() > 0) {
+                    hasName = true;
+                }
+                else {
+                    hasName = false;
+                }
+                setButtonVisibility();
+            }
+        };
 
         //Enter the name of the pet
         etInputName = findViewById(R.id.choose_et_inputName);
+        etInputName.addTextChangedListener(textWatcher);
+
+
 
         //Sends the pet data to the next page
         View.OnClickListener submit = new View.OnClickListener() {
@@ -69,7 +132,6 @@ public class ChoosePetActivity extends AppCompatActivity {
             DatabaseHelper dbHelper = new DatabaseHelper(ChoosePetActivity.this);
         };
 
-        Button submitBtn = findViewById(R.id.choose_btnSubmit);
         submitBtn.setOnClickListener(submit);
 
     }
